c5cd3fef8aeb641a4bb3890998487eef18b7d1e8
Longxiao Zhang <zhanglongx@gmail.com>
2019-02-28 09:40:13 +0800

    iconv_ez.sh: fix minor bugs

--- iconv_ez.sh
+++ iconv_ez.sh
@@ -41,14 +41,17 @@ for f in $@; do
 
     encoding=`chardet3 $f`
 
+    # FIXME: deal with chardet3 confidence
     if [[ $encoding =~ 'GB2312' ]]; then
         echo "convert $f into utf-8 ... "
         conv_inplace gb2312 utf8 $f
+        continue
     fi
 
     if [[ $encoding =~ 'utf-8' ]]; then
         echo "convert $f into gb2312 ... "
         conv_inplace utf8 gb2312 $f
+        continue
     fi
 
     echo "passed $f with encoding: $encoding"
