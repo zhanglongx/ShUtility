6f2c7fec6fe50d66e6787c20772d5f7cbb279b83
Longxiao Zhang <zhanglongx@gmail.com>
2023-05-24 15:14:16 +0800

    videogen.sh: fix bitrate bug

--- ffmpeg/videogen.sh
+++ ffmpeg/videogen.sh
@@ -107,5 +107,5 @@ fi
 ffmpeg -re -f lavfi -i "aevalsrc=if(eq(floor(t)\,ld(2))\,st(0\,random(4)*3000+1000))\;st(2\,floor(t)+1)\;st(1\,mod(t\,1))\;(0.6*sin(1*ld(0)*ld(1))+0.4*sin(2*ld(0)*ld(1)))*exp(-4*ld(1)) [out1]; testsrc=s=$VIDEOSIZE:rate=$RATE,drawtext=borderw=5:fontcolor=white:fontsize=30:text='%{localtime}/%{pts\:hms}':x=\(w-text_w\)/2:y=\(h-text_h-line_h\)/2 [out0]" \
       -acodec $ACODEC -vcodec $VCODEC -pix_fmt yuv420p -g $RATE \
       $XCODECPARAM \
-      -b:v $BITRATE -bufsize `echo $BITRATE | perl -ne 'printf "%.0f%s", $1 * 1.5, $2 if /(\d+)([kmg])/i'` \
+      -b:v $BITRATE -bufsize `echo $BITRATE | perl -ne 'printf "%.0f%s", $1 * 1.5, $2 if /(\d+)([kmg]*)/i'` \
       ${UNKNOWN[@]}
